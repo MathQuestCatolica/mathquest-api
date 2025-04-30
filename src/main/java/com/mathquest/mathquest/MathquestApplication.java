47c83736237e9abce1afc167e1382a08b4a16bf9
Otavio Augusto dos Santos <oaugusto@weg.net>
2025-04-29 21:18:07 -0300

    MTQ-89 feat: add first backend project files

---
+++ src/main/java/com/mathquest/mathquest/MathquestApplication.java
@@ -0,0 +1,13 @@
+package com.mathquest.mathquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathquestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathquestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mathquest/mathquest/MathquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathquest.mathquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathquestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
