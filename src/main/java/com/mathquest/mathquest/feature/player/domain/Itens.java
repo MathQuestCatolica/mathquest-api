a793981168bc620c7d406af9387e4d8e3e0373b4
João Dalri <105470028+Dalri05@users.noreply.github.com>
2025-05-06 19:41:06 -0300

    rename items

--- src/main/java/com/mathquest/mathquest/feature/player/domain/Itens.java
+++ src/main/java/com/mathquest/mathquest/feature/player/domain/Itens.java
@@ -1,16 +1,13 @@
 package com.mathquest.mathquest.feature.player.domain;
 
 import com.mathquest.mathquest.feature.player.enums.TipoItemEnum;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
-public class Items {
+@Table(name = "tb_itens")
+public class Itens {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
